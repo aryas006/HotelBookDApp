c452f9b3e5df830f47552abe70e56a1e5c5870fd
Arya <128399498+aryas006@users.noreply.github.com>
2024-01-11 16:22:18 +0530

    Add files via upload

---
+++ HotelBook.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HotelRoom {
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+    Statuses public currentStatus;
+    address payable public owner;
+    event Occupy(address _occupant, uint256 _value);
+
+    constructor() {
+        owner = payable(msg.sender);
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant() {
+        require(currentStatus == Statuses.Vacant, "currently occupied");
+        _;
+    }
+    modifier costs(uint256 _amount) {
+        require(msg.value >= _amount, "please add more ether");
+        _;
+    }
+
+    function book() public payable onlyWhileVacant costs(2 ether) {
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        (bool sent,bytes memory data)=owner.call{value:msg.value}("");
+        require(true);
+        emit Occupy(msg.sender, msg.value);
+    }
+}
